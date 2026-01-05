73550a22a864c682be5477f3f156df26d37cf64b
dhaksha-builds <dhaksha.flames@gmail.com>
2026-01-05 19:47:25 +0530

    Create planner.py

---
+++ planner.py
@@ -0,0 +1,22 @@
+class Planner:
+  def __init__(self, llm, plan_template):
+    self.llm = llm
+    self.plan_template = plan_template
+
+  def run(self, question, observation):
+    print("\n\n")
+    print("Plan Called")
+    print("\n\n")
+    print("Plan Template")
+    print("\n\n")
+    plan = self.llm(
+        prompt=self.plan_template.format(question=question, observation=observation),
+        max_tokens=4096,
+        temperature=0,
+        top_p=0.95,
+        repeat_penalty=1.2,
+        echo=False
+    )
+    output = plan["choices"][0]["text"].strip()
+    print(output)
+    return output
